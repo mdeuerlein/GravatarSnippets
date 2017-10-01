2a7b03ad3e6dd00c2589ded22d769fb77ff6153c
mdeuerlein <deuerlein@entidia.de>
2017-10-01 16:33:56 +0200

    test

--- php/gravatar_image_url.php
+++ php/gravatar_image_url.php
@@ -4,10 +4,11 @@
      *
      * Create URL of Gravatar User Image with fallback to a default image
 	   * @author Markus Deuerlein, https://entidia.de
-     *
+     * @TODO +feature teste
+     * // TODO This is a task +feature
     **/
 
-    $mailHash    = md5(strtolower( 'user@email.com' )); 
+    $mailHash    = md5(strtolower( 'user@email.com' ));
     $defaultImg  = md5(strtolower( trim('https://www.domain.com/images/default.jpg') ));
     $gravatarUrl = 'https://www.gravatar.com/avatar/'.$mailHash.'?d='.$imageUrl;
 ?>
